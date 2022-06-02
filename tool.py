17dd625caa3c4d8c73597b5666175f1142c6c0fe
Victor Porton <porton.victor@gmail.com>
2022-06-02 21:13:55 +0300

    rename

--- tool.py
+++ tool.py
@@ -12,7 +12,7 @@ group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("-f", "--zim-file", dest="zim_file", help="ZIM file for extraction")
 group.add_argument("-u", "--zim-url", dest="zim_url", help="ZIM URL to download")
 group.add_argument("-i", "--input-dir", dest="input_dir", help="input directory for upload to Swarm", metavar="DIR")
-parser.add_argument("-B", "--brotli", dest="brotli", help="compress files with Brotli (without change of extensions)", action=argparse.BooleanOptionalAction)
+parser.add_argument("-B", "--brotli", dest="brotli", help="compress files with Brotli (inplace)", action=argparse.BooleanOptionalAction)
 parser_extract = subparsers.add_parser('extract', help="extract from ZIM")
 parser_extract.add_argument("-o", "--output-dir", dest="output_dir", help="output directory", metavar="DIR")
 parser_upload = subparsers.add_parser('upload', help="upload to Swarm (after extraction if ZIM file specified)")
@@ -29,13 +29,9 @@ if args.command == 'extract' and args.input_dir is not None:
 
 no_upload = args.output_dir is not None
 
-zim_url = args.zim_url
-keepalive_seconds = args.keepalive_seconds
-zim_file = args.zim_file
-
-def prepare_files(output_dir):
+def extract_zim(output_dir):
     with TemporaryDirectory() as input_dir:
-        if zim_url:
+        if args.zim_url:
             # TODO: Don't place input.zim in current directory.
             os.system(f"wget -O {input_dir}/input.zim \"{zim_url}\"")
         else:
@@ -59,51 +55,55 @@ def prepare_files(output_dir):
         os.system(f"cp index.html error.html {output_dir}")
         # os.system(f"cd {output_dir} && wget https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css")
 
-        if args.brotli:
-            os.system(f"docker build -t brotler -f Dockerfile.brotler .")
-            print("Starting brotler...")
-            os.system(f"docker run --name brotler -v \"{abspath(output_dir)}:/volume\" brotler" \
-                f" /root/brotler/target/release/brotler /volume")
-            os.system(f"docker rm -f brotler")
-            os.system(f"sudo chown -R `id -u`:`id -g` {output_dir}")  # hack
-
-def full_upload():
+def extract_and_upload():
     with TemporaryDirectory() as tmpdir:
-        prepare_files(f"{tmpdir}/out")
+        extract_zim(tmpdir)
+        upload(tmpdir)
 
-        if args.batch_id is None:
-            price_in_bzz_per_byte_second = FIXME
-            total_size_in_bytes = int( subprocess.check_output(f"du -sb {tmpdir}/out | awk '{{print \$1}}'") )
-            cost_in_bzz = price_in_bzz_per_byte_second * total_size_in_bytes * keepalive_seconds
-            depth = 20
-            amount = cost_in_bzz * (10**16) / (2**depth)
-            res = requests.post(f"http://localhost:1635/stamps/{amount}/{depth}")
-            batch_id = res.json()["batchID"]  # FIXME: batch ID may be usable not immediately
+def upload(directory):
+    if args.brotli:
+        os.system(f"docker build -t brotler -f Dockerfile.brotler .")
+        print("Starting brotler...")
+        os.system(f"docker run --name brotler -v \"{abspath(output_dir)}:/volume\" brotler" \
+            f" /root/brotler/target/release/brotler /volume")
+        os.system(f"docker rm -f brotler")
+        os.system(f"sudo chown -R `id -u`:`id -g` {output_dir}")  # hack
 
-        res = requests.post("http://localhost:1633/tags")
-        tag = res.json()["uid"]
+    if args.batch_id is None:
+        price_in_bzz_per_byte_second = FIXME
+        total_size_in_bytes = int( subprocess.check_output(f"du -sb {directory} | awk '{{print \$1}}'") )
+        cost_in_bzz = price_in_bzz_per_byte_second * total_size_in_bytes * args.keepalive_seconds
+        depth = 20
+        amount = cost_in_bzz * (10**16) / (2**depth)
+        res = requests.post(f"http://localhost:1635/stamps/{amount}/{depth}")
+        batch_id = res.json()["batchID"]  # FIXME: batch ID may be usable not immediately
 
-        print("Starting TAR upload...")
-        process = subprocess.Popen(f"tar -C {tmpdir}/out -cf .", stdout=subprocess.PIPE)
-        res = requests.post("http://localhost:1633/bzz", data=tar, headers={
-            "Content-Type": "application/x-tar",
-            "Swarm-Index-Document": "index.html",
-            "Swarm-Error-Document": "error.html",
-            "Swarm-Collection": "true",
-            "Swarm-Postage-Batch-Id": batch_id,
-            "Swarm-Tag": tag,
-        })
+    res = requests.post("http://localhost:1633/tags")
+    tag = res.json()["uid"]
 
-        while True:
-            res = requests.get(f"http://localhost:1633/tags/{tag}")
-            total = res.json()['total']
-            processed = res.json()['processed']
-            synced = res.json()['synced']
-            print(f"fotal={total} processed={processed} synced={synced}")
-            if synced == total:
-                break
+    print("Starting TAR upload...")
+    process = subprocess.Popen(f"tar -C {directory} -cf .", stdout=subprocess.PIPE)
+    res = requests.post("http://localhost:1633/bzz", data=tar, headers={
+        "Content-Type": "application/x-tar",
+        "Swarm-Index-Document": "index.html",
+        "Swarm-Error-Document": "error.html",
+        "Swarm-Collection": "true",
+        "Swarm-Postage-Batch-Id": batch_id,
+        "Swarm-Tag": tag,
+    })
 
-if no_upload:
-    prepare_files(args.output_dir)
+    while True:
+        res = requests.get(f"http://localhost:1633/tags/{tag}")
+        total = res.json()['total']
+        processed = res.json()['processed']
+        synced = res.json()['synced']
+        print(f"fotal={total} processed={processed} synced={synced}")
+        if synced == total:
+            break
+
+if args.command == 'extract':
+    extract_zim(args.output_dir)
+elif args.input_dir:
+    upload(args.input_dir)
 else:
-    full_upload()
+    extract_and_upload()
