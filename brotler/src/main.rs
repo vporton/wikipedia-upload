dbefaddfc9722841bdb0736510e7d2726d137578
Victor Porton <porton.victor@gmail.com>
2022-06-02 12:30:18 +0300

    brotler created

--- brotler/src/main.rs
+++ brotler/src/main.rs
@@ -1,24 +1,36 @@
 use std::{env, process};
 use std::fmt::{Display, Formatter};
+use std::fs::{File, rename};
+use std::io::copy;
 use std::path::Path;
-use walkdir::{Error, WalkDir};
+use brotlic::CompressorWriter;
+use tempfile::{NamedTempFile, TempPath};
+use walkdir::WalkDir;
 
 #[derive(Debug)]
 enum MyError {
+    IO(std::io::Error),
     WalkDir(walkdir::Error),
 }
 
 impl Display for MyError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
+            Self::IO(err) => write!(f, "I/O: {err}"),
             Self::WalkDir(err) => write!(f, "Walking dir: {err}"),
         }
     }
 }
 
 impl From<walkdir::Error> for MyError {
-    fn from(value: Error) -> Self {
+    fn from(value: walkdir::Error) -> Self {
         Self::WalkDir(value)
+    }
+}
+
+impl From<std::io::Error> for MyError {
+    fn from(value: std::io::Error) -> Self {
+        Self::IO(value)
     }
 }
 
@@ -40,12 +52,20 @@ fn almost_main() -> Result<(), MyError> {
         .filter_entry(|e| !e.path_is_symlink())
     {
         let entry = entry?;
-        compress_file(&entry.path())
+        compress_file(&entry.path())?;
     }
 
     Ok(())
 }
 
-fn compress_file(path: &Path) {
-
+fn compress_file(path: &Path) -> Result<(), MyError> {
+    let mut input = File::open(path.clone())?; // uncompressed text file
+    let output_file = NamedTempFile::new()?;
+    let output_path: TempPath = output_file.into_temp_path();
+    let output_path: &Path = output_path.as_ref();
+    let output = File::create(output_path)?; // compressed text output file
+    let mut output_compressed = CompressorWriter::new(output);
+    copy(&mut input, &mut output_compressed)?;
+    rename(output_path, path)?;
+    Ok(())
 }
