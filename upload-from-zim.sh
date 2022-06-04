f49a5f3ad1cff93be5f6426a83bf7002f1a6cad7
Victor Porton <porton.victor@gmail.com>
2022-06-04 11:26:13 +0300

    bug fix

--- upload-from-zim.sh
+++ upload-from-zim.sh
@@ -7,4 +7,4 @@ if test $# != 2; then
   exit 1
 fi
 
-./tool.py -F static -H -M "$1" -S -B -m 400 $1 $2 upload -s $((30*24*3600)) -I index.html -I error.html
+./tool.py -F static -H -M "$1" -S -B -m 400 $1 $2 upload -s $((30*24*3600)) -I index.html -E error.html
