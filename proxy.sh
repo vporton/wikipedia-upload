7fb088a7b3e7cc7818427f6dc32ccacf97aeccd6
Victor Porton <porton.victor@gmail.com>
2022-06-05 06:21:02 +0300

    bug fix

--- proxy.sh
+++ proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run --rm proxy /root/proxy/target/release/proxy --port 8080 http://localhost:1633 \
+docker run --net=host --rm proxy /root/proxy/target/release/proxy --port 8080 http://localhost:1633 \
     -A "Content-Encoding: br" -R "Accept-Ranges" -R "Content-Length" -R "Decompressed-Content-Length"
