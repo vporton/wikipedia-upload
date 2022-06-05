f9e30c0d9bc180ae4c11179bcd2d6257fa63d0f8
Victor Porton <porton.victor@gmail.com>
2022-06-05 05:49:04 +0300

    proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker run --rm /root/proxy/target/release/proxy -p 8080 -A "Content-Encoding: br" -R Accept-Ranges -R Content-Length -R Decompressed-Content-Length
+docker run --rm /root/proxy/target/release/proxy -p 8080 -u http://localhost:1633 \
+    -A "Content-Encoding: br" -R "Accept-Ranges" -R "Content-Length" -R "Decompressed-Content-Length"
