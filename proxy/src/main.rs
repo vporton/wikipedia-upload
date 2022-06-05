431b07ef566d0c4444a61479f7c9a698dc89c89b
Victor Porton <porton.victor@gmail.com>
2022-06-05 18:00:20 +0300

    proxy/ simplified

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,33 +1,13 @@
-use std::collections::HashSet;
 use std::fmt::{Display, Formatter};
 use actix_web::{get, App, HttpServer, Responder, HttpRequest, ResponseError, HttpResponse};
 use actix_web::http::header::ContentType;
 use actix_web::web::Data;
 use clap::Parser;
-use reqwest::header::{HeaderName, HeaderValue, InvalidHeaderName, InvalidHeaderValue};
-
-#[derive(Debug)]
-struct WrongHeaderError;
-
-impl WrongHeaderError {
-    pub fn new() -> Self {
-        Self { }
-    }
-}
-
-impl Display for WrongHeaderError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Wrong header specified")
-    }
-}
 
 #[derive(Debug)]
 enum MyError {
     Reqwest(reqwest::Error),
     IO(std::io::Error),
-    WrongHeader(WrongHeaderError),
-    InvalidHeaderName(InvalidHeaderName),
-    InvalidHeaderValue(InvalidHeaderValue),
     Actix(actix_web::Error),
 }
 
@@ -50,9 +30,6 @@ impl Display for MyError {
         match self {
             Self::Reqwest(err) => write!(f, "Upstream error: {err}"),
             Self::IO(err) => write!(f, "I/O error: {err}"),
-            Self::WrongHeader(err) => write!(f, "Header error: {err}"),
-            Self::InvalidHeaderName(err) => write!(f, "Header name error: {err}"),
-            Self::InvalidHeaderValue(err) => write!(f, "Header value error: {err}"),
             Self::Actix(err) => write!(f, "Actix error: {err}"),
         }
     }
@@ -64,27 +41,9 @@ impl From<reqwest::Error> for MyError {
     }
 }
 
-impl From<WrongHeaderError> for MyError {
-    fn from(value: WrongHeaderError) -> Self {
-        Self::WrongHeader(value)
-    }
-}
-
 impl From<std::io::Error> for MyError {
     fn from(value: std::io::Error) -> Self {
         Self::IO(value)
-    }
-}
-
-impl From<InvalidHeaderName> for MyError {
-    fn from(value: InvalidHeaderName) -> Self {
-        Self::InvalidHeaderName(value)
-    }
-}
-
-impl From<InvalidHeaderValue> for MyError {
-    fn from(value: InvalidHeaderValue) -> Self {
-        Self::InvalidHeaderValue(value)
     }
 }
 
@@ -102,45 +61,17 @@ struct Config {
     /// Our HTTP port
     #[clap(short = 'p', long = "--port")]
     port: u16,
-
-    /// Headers to remove
-    #[clap(short = 'R', long = "--remove")]
-    headers_to_remove: Vec<String>,
-
-    /// Headers to add
-    #[clap(short = 'A', long = "--add")]
-    headers_to_add: Vec<String>,
-}
-
-#[derive(Clone)]
-struct ConfigMore {
-    headers_to_remove_set: HashSet<String>,
-    headers_to_add: Vec<(String, String)>,
 }
 
 #[get("/{path:.*}")]
-async fn proxy_get(req: HttpRequest, config: Data<Config>, config_more: Data<ConfigMore>)
+async fn proxy_get(req: HttpRequest, config: Data<Config>)
     -> Result<impl Responder, MyError>
 {
     let client = reqwest::Client::new();
-    let mut headers = reqwest::header::HeaderMap::new();
-    for (key, value) in req.headers().iter() {
-        let key = key.to_string().to_lowercase();
-        if !config_more.headers_to_remove_set.contains(&key) {
-            headers.insert(HeaderName::from_bytes(key.as_bytes())?, value.clone());
-        }
-    }
-    for (key, value) in &config_more.headers_to_add {
-        headers.insert(HeaderName::from_bytes(key.as_bytes())?, HeaderValue::from_bytes(value.as_bytes())?);
-    }
     let reqwest_response = client.get(config.upstream.clone() + req.path())
-        .headers(headers.clone())
         .send()
         .await?;
     let mut response_builder = HttpResponse::build(actix_web::http::StatusCode::OK);
-    for (key, value) in headers {
-        response_builder.append_header((key.unwrap(), value));
-    }
     Ok(response_builder.streaming(reqwest_response.bytes_stream()))
 }
 
@@ -148,22 +79,10 @@ async fn proxy_get(req: HttpRequest, config: Data<Config>, config_more: Data<Con
 async fn main() -> Result<(), MyError> {
     let config = Config::parse();
     let config2 = config.clone();
-    let config3 = config.clone();
-    let config_more = ConfigMore {
-        headers_to_remove_set: HashSet::from_iter(config.headers_to_remove.into_iter().map(|k| k.to_lowercase())),
-        headers_to_add: config.headers_to_add
-            .into_iter()
-            .map(|h| -> Result<_, MyError> {
-                let p = h.split_once(": ").ok_or::<MyError>(WrongHeaderError::new().into())?;
-                Ok((p.0.to_string(), p.1.to_string()))
-            })
-            .collect::<Result<Vec<(String, String)>, MyError>>()?,
-    };
 
     Ok(HttpServer::new(move ||
         App::new()
-            .app_data(Data::new(config3.clone()))
-            .app_data(Data::new(config_more.clone()))
+            .app_data(Data::new(config.clone()))
             .service(proxy_get)
     )
         .bind(("127.0.0.1", config2.port))?
