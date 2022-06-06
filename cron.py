2253f8e8bc218d7d8e8f2f8f59ad1537b1f1fe16
Victor Porton <porton.victor@gmail.com>
2022-06-07 01:38:04 +0300

    cron.py ready?

---
+++ cron.py
@@ -0,0 +1,33 @@
+#!/bin/env python3
+
+import os
+import re
+import sys
+import requests
+
+
+if len(sys.argv) != 4:
+    print("A specialized script to upload Wikipedia to Swarm\n")
+    print("Usage ./cron.py <SOURCE-DIR> <STATE-DIR> <SUBSTR>")
+    print("\nHere <SOURCE-DIR> is this directory, <STATE-DIR> stores the state and log, <SUBSTR> is like `en_all_maxi`.")
+    sys.exit(1)
+source_dir, state_dir, sstr = sys.argv[1], sys.argv[2], sys.argv[3]
+
+directory_url = "https://dumps.wikimedia.org/other/kiwix/zim/wikipedia/"
+
+res = requests.get(directory_url)
+matches = re.findall('href="(.*?)"', res.content.decode('utf-8'))
+matches = [match for match in matches if re.match(f'^wikipedia_{sstr}_([0-9]{{4}})-([0-9]{{2}}).zim$', match)]
+url = directory_url + max(matches)
+
+try:
+    with open(f"{state_dir}/last_upload.txt", "r") as f:
+        old_url = f.read()
+        if old_url == url:
+            print(f"{url} already uploaded, exiting")
+except FileNotFoundError:
+    pass
+
+# Without multiplying by 10 upload stalls.
+os.system*f"{source_dir}/tool.py -F {source_dir}/static -H -M '' -S -B -m 400 -u {url} upload " \
+    f"-s {10*30*24*3600} -I {source_dir}/index.html -E {source_dir}/error.html -L {state_dir}/uploads.log")
