1485cb189b9886978bf4c570de704b8087f5d70e
Victor Porton <porton.victor@gmail.com>
2022-06-07 01:43:05 +0300

    bug fixes

--- cron.py
+++ cron.py
@@ -28,6 +28,19 @@ try:
 except FileNotFoundError:
     pass
 
+class CommandError(Exception):
+    """Error running shell command"""
+
+    def __init__(self, command):
+        super().__init__(f"Error running shell command: {command}")
+
+def run_command(command):
+    if os.system(command) != 0:
+        raise CommandError(command)
+
 # Without multiplying by 10 upload stalls.
-os.system*f"{source_dir}/tool.py -F {source_dir}/static -H -M '' -S -B -m 400 -u {url} upload " \
+run_command(f"{source_dir}/tool.py -F {source_dir}/static -H -M '' -S -B -m 400 -u {url} upload " \
     f"-s {10*30*24*3600} -I {source_dir}/index.html -E {source_dir}/error.html -L {state_dir}/uploads.log")
+
+with open(f"{state_dir}/last_upload.txt", "w") as f:
+    f.write(url)
